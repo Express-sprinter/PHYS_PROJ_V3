f70169dbb462eacab9c552fc55a8c89f91372e5d
Express-sprinter <98044473+Express-sprinter@users.noreply.github.com>
2025-10-31 08:54:54 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  base: '/REPO_NAME/',  // <-- replace REPO_NAME with your GitHub repo name
+  base: '/Express-sprinter/PHYS_PROJ_V3/',  // <-- replace REPO_NAME with your GitHub repo name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
